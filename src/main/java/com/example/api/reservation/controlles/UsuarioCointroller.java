4ca96714b1f29b9d92987206d765250a3aeae301
jonatas <jonatas_dev@hotmail.com>
2026-01-19 15:33:13 -0300

    TODO: Created Usuario Controller

---
+++ src/main/java/com/example/api/reservation/controlles/UsuarioCointroller.java
@@ -0,0 +1,40 @@
+package com.example.api.reservation.controlles;
+
+import com.example.api.reservation.models.UsuarioModel;
+import com.example.api.reservation.services.UsuarioService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioCointroller {
+
+    private final UsuarioService usuarioService;
+
+    public UsuarioCointroller(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @PostMapping
+    public ResponseEntity<UsuarioModel> criarUsuario(@RequestBody UsuarioModel usuario) {
+        return ResponseEntity.ok(usuarioService.criandoUsuario(usuario));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UsuarioModel> buscaPorId(@PathVariable("id")UUID id) {
+        return ResponseEntity.ok(usuarioService.buscarUsuarioPorId(id));
+    }
+    @GetMapping
+    public ResponseEntity<List<UsuarioModel>> listarTodos(){
+        return ResponseEntity.ok(usuarioService.listarTodosOsUsuarios());
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<UsuarioModel> atualizarPorId(@PathVariable("id") UUID id ,@RequestBody UsuarioModel usuario){
+        return ResponseEntity.ok(usuarioService.atualizarUsuario(usuario));
+    }
+
+}
